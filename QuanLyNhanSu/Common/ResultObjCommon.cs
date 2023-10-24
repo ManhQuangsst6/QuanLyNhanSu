3e3b16335870f5b43d465164dbf9b72506d1780f
ManhQuangsst6 <daoquangsst6@gmail.com>
2023-10-24 14:30:47 +0700

    add img

---
+++ QuanLyNhanSu/Common/ResultObjCommon.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanLyNhanSu.Common
+{
+	public class ResultObjCommon<T>
+	{
+		public T? Obj { get; set; }
+		public bool Success { set; get; } = true;
+		public string? Message { get; set; }
+	}
+}
