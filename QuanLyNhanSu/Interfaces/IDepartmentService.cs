c1cece2876ad048a7c7d7cf63d92737c2529120f
ManhQuangsst6 <daoquangsst6@gmail.com>
2023-10-24 14:30:47 +0700

    Add project files.

---
+++ QuanLyNhanSu/Interfaces/IDepartmentService.cs
@@ -0,0 +1,10 @@
+﻿using QuanLyNhanSu.Models;
+
+namespace QuanLyNhanSu.Interfaces
+{
+	public interface IDepartmentService
+	{
+		Task<List<Department>> GetAllDepartment();
+		Task<Department> AddDepartment();
+	}
+}
---
+++ QuanLyNhanSu/Models/ModelDTO/EmployeeView.cs
@@ -0,0 +1,7 @@
+﻿namespace QuanLyNhanSu.Models.ModelDTO
+{
+	public class EmployeeView
+	{
+		// Các trường nhân viên cần hiển thị  
+	}
+}
