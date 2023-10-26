6d6fd78fc1c0d818b834bc4fe267aaadba59c0f0
hung201200146@lms.utc.edu.vn <hung201200146@lms.utc.edu.vn>
2023-10-26 22:22:22 +0700

    Model

---
+++ QuanLyNhanSu/Models/Employee_Project.cs
@@ -0,0 +1,12 @@
+﻿namespace QuanLyNhanSu.Models
+{
+	public class Employee_Project
+	{
+		public string EmployeeID { get; set; }
+		public string ProjectID { get; set; }
+		public double MoneyBonus { get; set; }
+		public DateTime? DateStart { get; set; }
+		public DateTime? DateEnd { get; set; }
+
+	}
+}
