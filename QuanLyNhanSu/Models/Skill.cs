632a9ef783fe001a5a49976bdd19d97f88ea5a77
hung201200146@lms.utc.edu.vn <hung201200146@lms.utc.edu.vn>
2023-10-26 22:22:31 +0700

    Model

---
+++ QuanLyNhanSu/Models/Skill.cs
@@ -0,0 +1,10 @@
+﻿namespace QuanLyNhanSu.Models
+{
+	public class Skill
+	{
+		public string ID { get; set; }
+		public string Name { get; set; }
+		public string? Description { get; set; }
+
+	}
+}
