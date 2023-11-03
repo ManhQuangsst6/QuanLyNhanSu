7d1a5ee6a463355fb61b66bccaca3b969204ef29
DaoQuangsst6 <DaoQuangsst6@gmail.com>
2023-11-04 01:45:36 +0700

    oll

--- QuanLyNhanSu/Controllers/ListViewController.cs
+++ QuanLyNhanSu/Controllers/ListViewController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyNhanSu.Interfaces;
 
 namespace QuanLyNhanSu.Controllers
 {
@@ -6,77 +7,77 @@ namespace QuanLyNhanSu.Controllers
 	[ApiController]
 	public class ListViewController : ControllerBase
 	{
-		//private readonly IViewService viewService;
-		//public ListViewController(IViewService viewService)
-		//{
-		//	this.viewService = viewService;
-		//}
-		//[HttpGet]
-		//public async Task<IActionResult> GetView_DepartmentList()
-		//{
-		//	try
-		//	{
-		//		var res = await viewService.GetView_DepartmentList();
-		//		return Ok(res);
-		//	}
-		//	catch
-		//	(Exception ex)
-		//	{
-		//		return BadRequest(ex.Message);
-		//	}
+		private readonly IViewService viewService;
+		public ListViewController(IViewService viewService)
+		{
+			this.viewService = viewService;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetView_DepartmentList()
+		{
+			try
+			{
+				var res = await viewService.GetView_DepartmentList();
+				return Ok(res);
+			}
+			catch
+			(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 
 
 
-		//}
-		//[HttpGet]
-		//public async Task<IActionResult> GetView_ProjectList()
-		//{
-		//	try
-		//	{
-		//		var res = await viewService.GetView_ProjectList();
-		//		return Ok(res);
-		//	}
-		//	catch
-		//	(Exception ex)
-		//	{
-		//		return BadRequest(ex.Message);
-		//	}
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetView_ProjectList()
+		{
+			try
+			{
+				var res = await viewService.GetView_ProjectList();
+				return Ok(res);
+			}
+			catch
+			(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 
-		//}
-		//[HttpGet]
-		//public async Task<IActionResult> GetView_SkillList()
-		//{
-		//	try
-		//	{
-		//		var res = await viewService.GetView_SkillList();
-		//		return Ok(res);
-		//	}
-		//	catch
-		//	(Exception ex)
-		//	{
-		//		return BadRequest(ex.Message);
-		//	}
-
-
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetView_SkillList()
+		{
+			try
+			{
+				var res = await viewService.GetView_SkillList();
+				return Ok(res);
+			}
+			catch
+			(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 
 
 
-		//}
 
-		//[HttpGet]
-		//public async Task<IActionResult> GetView_PositionList()
-		//{
-		//	try
-		//	{
-		//		var res = await viewService.GetView_PositionList();
-		//		return Ok(res);
-		//	}
-		//	catch
-		//	(Exception ex)
-		//	{
-		//		return BadRequest(ex.Message);
-		//	}
 
-		//}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetView_PositionList()
+		{
+			try
+			{
+				var res = await viewService.GetView_PositionList();
+				return Ok(res);
+			}
+			catch
+			(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+
+		}
 	}
 }
